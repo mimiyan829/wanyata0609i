91ed3ed38220bb4fa3ff9ef308a8a5bf5cc2f4e1
曹雪 <2064753679@qq.com>
2022-06-09 11:07:33 +0800

    mimiyan类test类第一次提交

---
+++ mimiyan.java
@@ -0,0 +1,6 @@
+public class mimiyan {
+    public static void main(String[] args) {
+        System.out.println("XYG电子竞技俱乐部");
+        System.out.println("万亚太");
+    }
+}
---
+++ test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("曹雪");
+        System.out.println("201917824");
+        System.out.println();
+    }
+}
