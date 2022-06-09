93a09c3e57c6260960b49517faeaf8b5e72e1a73
曹雪 <2064753679@qq.com>
2022-06-09 11:09:26 +0800

    test类第二次提交

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("曹雪");
         System.out.println("201917824");
-        System.out.println();
+        System.out.println("万亚太");
     }
 }
